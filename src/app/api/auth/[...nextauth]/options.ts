1ed66f4af58bad6b811002ee096075fb3dc54f10
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-03-25 14:04:59 +0500

    credentials

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,4 +2,39 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/model/User.model"
+import UserModel from "@/model/User.model";
+
+export const authOptions:NextAuthOptions ={
+    providers:[
+        CredentialsProvider(
+            {
+                id:"credentials",
+                name:"Credentials",
+                credentials: {
+                    email: { label: "Email", type: "text" },
+                    password: { label: "Password", type: "password" }
+                  },
+                  async authorize(credentials:any, req):Promise<any>{
+                    await dbConnect();
+                    try {
+                        const user = await UserModel.findOne({
+                            $or:[
+                                {email:credentials.identifier},
+                                {username:credentials.identifier}
+                            ]
+                        })
+                        if(!user){
+                            throw new Error("No User Found!")
+                        }
+                        if(!user.isVerified){
+                            throw new Error("Please verified your account first");
+                        }
+                    } catch (error:any) {
+                        throw new Error(error);
+                    }
+                  }
+
+            }
+        )
+    ]
+}
