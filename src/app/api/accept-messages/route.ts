3113e5eeefa85001317920eef245414c4675b8c9
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-05-12 10:18:36 +0500

    update

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -2,18 +2,26 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import UserModel from "@/model/User.model";
 import dbConnect from "@/lib/dbConnect";
-import {User} from 'next-auth'
+import { User } from 'next-auth'
 
-export async function POST(request:Request)
-{
+export async function POST(request: Request) {
 
     await dbConnect();
 
     try {
-        const session  = await getServerSession(authOptions);
-        const user:User = session?.user;
+        const session = await getServerSession(authOptions);
+        const user: User = session?.user;
+        if (!session || !session.user) {
+            return Response.json({
+                success: false,
+                message: "Not Authenticated"  
+            },
+                {
+                    status: 401
+                })
+        }
     } catch (error) {
-        
+
     }
-    
+
 }
